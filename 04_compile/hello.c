15def2beedded20c060966a4c410b4fabe17a79c
Learn 2 Program Grader <invalid@nowhere.com>
2020-03-09 23:36:06 +0000

    Did assignment 4

--- 04_compile/hello.c
+++ 04_compile/hello.c
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(void) {
-  printf("Hello World");
-
-=======
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
   printf("Hello World\n");
->>>>>>> 940497532c91e2965c46ee4b5e0ca831e77783ef
   return EXIT_SUCCESS;
 }
